ff1001df7a3c1a4fe431b76de0aceb1e1dfb8ed9
yeasirarafatpp <yeasira79@gmail.com>
2021-10-29 20:52:50 +0600

    connect db

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8888;
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdoqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/', (req, res) => {
     res.send('Hi I am Database');
