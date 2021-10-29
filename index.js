381d7f9126a0457d769c02765d8a42dec4c101fc
yeasirarafatpp <yeasira79@gmail.com>
2021-10-29 22:17:25 +0600

    get all data from db

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 8888;
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
+// Middleware
+app.use(cors());
+app.use(express.json);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdoqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
