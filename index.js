2e22e0f52ee1c61eae81204aca30a16b405aebf0
yeasirarafatpp <yeasira79@gmail.com>
2021-10-29 22:02:46 +0600

    get all data from db

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ const { MongoClient } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdoqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-console.log(uri)
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("Tourdestination");
+        const toursCollection = database.collection("tours");
+        // GET API
+        app.get('/tours', async (req, res) => {
+            const cursor = toursCollection.find({});
+            const tours = await cursor.toArray();
+            res.send(tours);
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hi I am Database');
