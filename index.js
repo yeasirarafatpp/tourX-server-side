1c7dfbc5e11a1ac15752c1cb22cc65d5cba72651
yeasirarafatpp <yeasira79@gmail.com>
2021-10-31 01:12:35 +0600

    api generate

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 // Middleware
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wdoqy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
